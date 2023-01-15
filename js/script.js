07e59becd31c31ff7f016212446198ba2bde5d6f
Enjamul Islam <mdenjamulislam527@gmail.com>
2023-01-15 10:00:31 +0600

    Commenting

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-// Testimonial Slider
+/*-------------------------------------------------
+
+    Template name: E-book Landing Page Template
+    Template URI: https://enjamul.me/E-book-landing-page-template
+    Author Name: Enjamul Islam
+    Author URI: https://enjamul.me
+    Description: Our e-book landing page template is designed to help authors and publishers promote and sell their digital books with a clean, modern, and optimized design that converts visitors into customers. The template includes a prominent call-to-action button, information about the book and author, and space for reviews and testimonials. It's fully customizable and easy to use.
+    Version: 1.0
+
+-------------------------------------------------
+
+    JS Index
+=======================
+
+    1.0. Testimonial Slider
+
+-------------------------------------------------*/
 
 "use strict";
+
+/*================================================
+    1.0. Testimonial Slider
+================================================*/
 
 var slider = tns({
     container: '.my-slider',
